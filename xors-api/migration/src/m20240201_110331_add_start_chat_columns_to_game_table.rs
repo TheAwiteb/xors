6bafbd43eeaba9256de4f27e5b42dda4fbf5da74
Awiteb <Awiteb@pm.me>
2024-02-01 14:18:51 +0300

    Add start chat columns to game table

--- xors-api/migration/src/m20240201_110331_add_start_chat_columns_to_game_table.rs
+++ xors-api/migration/src/m20240201_110331_add_start_chat_columns_to_game_table.rs
@@ -14,29 +14,28 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-use sea_orm::entity::prelude::*;
+use sea_orm_migration::prelude::*;
 
-#[derive(Clone, Debug, PartialEq, DeriveEntityModel, Eq)]
-#[sea_orm(table_name = "game")]
-pub struct Model {
-    #[sea_orm(primary_key)]
-    pub id: i32,
-    pub uuid: Uuid,
-    pub round: i16,
-    pub auto_play_after: Option<chrono::NaiveDateTime>,
-    pub rounds_result: String,
-    pub x_player: Uuid,
-    pub o_player: Uuid,
-    pub x_start_chat: bool,
-    pub o_start_chat: bool,
-    pub board: String,
-    pub winner: Option<Uuid>,
-    pub reason: Option<String>,
-    pub created_at: chrono::NaiveDateTime,
-    pub ended_at: Option<chrono::NaiveDateTime>,
+use crate::m20240119_135153_game::Game;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .alter_table(
+                Table::alter()
+                    .table(Game::Table)
+                    .add_column(ColumnDef::new(Game::XStartChat).boolean().not_null())
+                    .add_column(ColumnDef::new(Game::OStartChat).boolean().not_null())
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, _manager: &SchemaManager) -> Result<(), DbErr> {
+        Ok(())
+    }
 }
-
-#[derive(Copy, Clone, Debug, EnumIter, DeriveRelation)]
-pub enum Relation {}
-
-impl ActiveModelBehavior for ActiveModel {}
